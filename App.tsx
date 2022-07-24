8e3e4b3302e43a0a91ded34579728b9993bef55a
FerTarallo <contato.fernandatarallo@gmail.com>
2022-07-24 16:41:32 -0300

    FEAT/add-routes-and-screens-details-home-register

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { SignIn } from './src/screens/SignIn';
+import { Details } from './src/screens/Details';
 import { Loading } from './src/components/Loading';
 
+import { Routes } from './src/routes';
 import { THEME } from './src/styles/theme';
 
 export default function App() {
@@ -16,7 +17,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
